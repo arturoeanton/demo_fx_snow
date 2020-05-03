6c1484e1aa3918cd112e05905bf9cf54e6ccb936
Arturo Elias Anton <arturoeanton@gmail.com>
2020-05-03 18:26:56 -0300

    update demo

---
+++ lib.js
@@ -0,0 +1,7 @@
+let  sum  = (s,d) => {
+    return s+d;
+};
+
+module.exports = {
+    sum: sum
+}
---
+++ main.js
@@ -0,0 +1,39 @@
+const lib = require('./lib.js')
+const m = require('./lib/m.js')
+
+class Foo {
+    get bar() {
+        return 1;
+    }
+}
+
+
+class Bar extends Foo {
+    bar() {
+        return 2;
+    }
+}
+
+let a = () => {
+    return v;
+};
+let d =678;
+
+let bar = new Bar();
+
+let o = {param0:"aa",d:"a"};
+o.aa=1
+let f1 = `->${d}`;
+let f2 = v;
+const string1 = "fooabar";
+const match = string1.match(/foo(.)bar/u);
+let v1 = [1,1000, bar.bar()];
+let e = '';
+eval("e = 'foo';");
+
+console.log(e);
+
+console.log("Started");
+let ss = lib.sum(1000,20);
+let payload = [...v1,{f1, f2, match, e, ss,o, aa:m.testA()}];
+
