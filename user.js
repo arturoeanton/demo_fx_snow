2da65608a7170b5aa2163c656d5975be0b4d7c94
Arturo Elias <arturoeanton@gmail.com>
2020-05-04 21:29:25 -0300

    Update user.js

--- user.js
+++ user.js
@@ -1,7 +1,7 @@
 let http_status = 200
 let get_user = () => {
     ///http_status=400
-    return  {msg: "user get",  vars, paso:"edgardo"};
+    return  {msg: "user get",  vars};
 }
 
 let post_user = () => {
